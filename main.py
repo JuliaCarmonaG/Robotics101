e3a0a47b011b80686dc18693523f7098e002b096
JuliaCarmonaG <julia.carmona0109@outlook.com>
2025-02-13 20:02:48 -0600

    Create main.py
    calcula el pemimetro de un circulo

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+
+def calcular_perimetro_circulo(radio):
+    return 2 * math.pi * radio
+
+# Ejemplo de uso
+radio = 5
+perimetro = calcular_perimetro_circulo(radio)
+print(f"El perímetro del círculo con radio {radio} es {perimetro}")
+
