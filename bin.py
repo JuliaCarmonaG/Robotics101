74190315ba304db25e5a338427b2b74eadad5a06
JuliaCarmonaG <julia.carmona0109@outlook.com>
2025-02-13 21:29:41 -0600

    Robot en movimiento
    Vectores robot en movimiento

---
+++ bin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import roboticstoolbox as rtb
+import numpy as np
+
+robot=rtb.models.DH.Puma560() 
+
+#Variables articulares
+q=[0, np.deg2rad(30), -np.deg2rad(160), 0, 0, 0]
+
+#Visualizar
+#robot.plot(q,block=True,backend='pyplot')
+#Si se downgradeo a matplotlib 3.8.3
+robot.teach(q)
+
+
--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import math
+import roboticstoolbox as rtb
+import numpy as np
 
-def calcular_perimetro_circulo(radio):
-    return 2 * math.pi * radio
-
-# Ejemplo de uso
-radio = 5
-perimetro = calcular_perimetro_circulo(radio)
-print(f"El perímetro del círculo con radio {radio} es {perimetro}")
-
-def calcular_area_circulo(radio):
-    return math.pi * radio ** 2
-
-def calcular_volumen_esfera(radio):
-    return (4/3) * math.pi * radio ** 3
-
-# Ejemplo de uso
-area = calcular_area_circulo(radio)
-volumen = calcular_volumen_esfera(radio)
-print(f"El área del círculo con radio {radio} es {area}")
-print(f"El volumen de la esfera con radio {radio} es {volumen}")
+print(f"Versión de roboticstoolbox: {rtb.__version__}")
+print(f"Versión de numpy: {np.__version__}")
