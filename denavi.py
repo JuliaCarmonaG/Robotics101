f5b4145a03252e4c0c353e6be7f5f047fb2dcf48
JuliaCarmonaG <julia.carmona0109@outlook.com>
2025-03-02 23:38:01 -0600

    Update denavi.py

--- denavi.py
+++ denavi.py
@@ -13,14 +13,15 @@ THD= trotz(theta) @ transl(0,0,d) @ transl(a,0,0) @ trotx(alpha)
 sp.pprint(THD)
 print(type(THD))
 
-T= sp.Matrix([[sp.cos(theta), -sp.sin(theta), 0, a*sp.cos(theta)],
-              [sp.sin(theta)*sp.cos(alpha), sp.cos(theta)*sp.cos(alpha), -sp.sin(alpha), -a*sp.sin(alpha)],
-              [sp.sin(theta)*sp.sin(alpha), sp.cos(theta)*sp.sin(alpha), sp.cos(alpha), d],
+T= sp.Matrix([[sp.cos(theta), -sp.sin(theta)*sp.cos(alpha), sp.sin(theta)*sp.sin(alpha), a*sp.cos(theta)],
+              [sp.sin(theta), sp.cos(theta)*sp.cos(alpha), -sp.cos(theta)*sp.sin(alpha), a*sp.sin(theta)],
+              [0, sp.sin(alpha), sp.cos(alpha), d],
               [0, 0, 0, 1]])
 sp.pprint(T)
 print(type(T))
 
 theta_1, theta_2, theta_3, theta_4, theta_5, theta_6 = sp.symbols('theta_1 theta_2 theta_3 theta_4 theta_5 theta_6')
+#usando la tabla DH
 T01= T.subs({d: 0.330, a: 0.050, alpha: -sp.pi/2})
 T01= T01.subs({theta: theta_1})
 sp.pprint(T01)
@@ -40,8 +41,19 @@ T56= T.subs({d: 0.080, a: 0, alpha: 0})
 T56= T56.subs({theta: theta_6})
 sp.pprint(T56)
 T06= T01 @ T12 @ T23 @ T34 @ T45 @ T56
-T06_s=T06.applyfunc(sp.simplify)
+T06_s= T06.applyfunc(sp.simplify)
 sp.pprint(T06_s)
 
-latex_code= sp.latex(T06_s)
-print(latex_str)
+#latex_str= sp.latex(T06_s)
+#print(latex_str)
+
+
+joint1= np.deg2rad(0)
+joint2= np.deg2rad(0)-sp.pi/2 #offset
+joint3= np.deg2rad(0)
+joint4= np.deg2rad(0)
+joint5= np.deg2rad(0)
+joint6= np.deg2rad(0)
+
+T06_solved=T06_s.subs({theta_1: joint1, theta_2: joint2, theta_3: joint3, theta_4: joint4, theta_5: joint5, theta_6: joint6})
+sp.pprint(T06_solved)
