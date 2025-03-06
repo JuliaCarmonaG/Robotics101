b719b0be6315916885c49b73589111b92c9ce9a6
JuliaCarmonaG <julia.carmona0109@outlook.com>
2025-03-05 20:01:23 -0600

    Update denavi.py

--- denavi.py
+++ denavi.py
@@ -22,38 +22,39 @@ print(type(T))
 
 theta_1, theta_2, theta_3, theta_4, theta_5, theta_6 = sp.symbols('theta_1 theta_2 theta_3 theta_4 theta_5 theta_6')
 #usando la tabla DH
-T01= T.subs({d: 0.330, a: 0.050, alpha: -sp.pi/2})
+T01= T.subs({d: .0674, a: 0, alpha: +sp.pi/2})
 T01= T01.subs({theta: theta_1})
 sp.pprint(T01)
-T12= T.subs({d: 0, a: 0.440, alpha: 0})
+T12= T.subs({d: 0.0644, a: .274, alpha: +sp.pi})
 T12= T12.subs({theta: theta_2})
 sp.pprint(T12)
-T23= T.subs({d: 0, a: 0.035, alpha: -sp.pi/2})
+T23= T.subs({d: 0, a: .230, alpha: +sp.pi})
 T23= T23.subs({theta: theta_3})
 sp.pprint(T23)
-T34= T.subs({d: 0.420, a: 0, alpha: sp.pi/2})
+T34= T.subs({d: 0.058, a: 0, alpha: -sp.pi/2})
 T34= T34.subs({theta: theta_4})
 sp.pprint(T34)
-T45= T.subs({d: 0, a: 0, alpha: -sp.pi/2})
+T45= T.subs({d: 0.116, a: 0, alpha: +sp.pi/2})
 T45= T45.subs({theta: theta_5})
 sp.pprint(T45)
-T56= T.subs({d: 0.080, a: 0, alpha: 0})
+T56= T.subs({d: 0.0565, a: 0, alpha: 0})
 T56= T56.subs({theta: theta_6})
 sp.pprint(T56)
 T06= T01 @ T12 @ T23 @ T34 @ T45 @ T56
 T06_s= T06.applyfunc(sp.simplify)
 sp.pprint(T06_s)
 
+
 #latex_str= sp.latex(T06_s)
 #print(latex_str)
 
-
-joint1= np.deg2rad(0)
-joint2= np.deg2rad(0)-sp.pi/2 #offset
-joint3= np.deg2rad(0)
-joint4= np.deg2rad(0)
-joint5= np.deg2rad(0)
+joint1= np.deg2rad(0) +sp.pi/2 #offset
+joint2= np.deg2rad(0)+sp.pi/2 #offset
+joint3= np.deg2rad(0) #offset
+joint4= np.deg2rad(0)-sp.pi/2 #offset|
+joint5= np.deg2rad(0) #offset
 joint6= np.deg2rad(0)
+
 
 T06_solved=T06_s.subs({theta_1: joint1, theta_2: joint2, theta_3: joint3, theta_4: joint4, theta_5: joint5, theta_6: joint6})
 sp.pprint(T06_solved)
